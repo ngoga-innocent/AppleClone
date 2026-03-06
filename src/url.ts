159d67e49ed2a1a381314b076ae104798899b998
Ngoga <Ngogainnocent1@gmail.com>
2026-03-06 16:12:14 +0200

    changed url

--- src/url.ts
+++ src/url.ts
@@ -1,2 +1,3 @@
 // export const url='http://localhost:8000'
-export const url='https://api.applepromo.info'
+// export const url='https://api.applepromo.info'
+export const url='https://applecloneapi.onrender.com'
